5db82806de9c8286b5f5f8d9783cb14c35901112
Costin <virlan.costin.andrei@gmail.com>
2023-07-28 12:55:45 +0300

    Fixed drmax

--- sites/drmax.js
+++ sites/drmax.js
@@ -1,65 +1,74 @@
 const { Scraper, postApiPeViitor } = require("peviitor_jsscraper");
+
 const generateJob = (job_title, job_link, city) => ({
   job_title,
   job_link,
   country: "Romania",
-  city, 
+  city,
 });
 
-const getJobs = async (url) => {
-  let jobs = [];
+const getJobBatch = async (url) => {
+  const jobs = [];
   const scraper = new Scraper(url);
   const type = "HTML";
   const soup = await scraper.get_soup(type);
   const soupElements = soup.findAll("h2", { class: "awsm-job-post-title" });
-  const soupElements2 = soup.findAll("div", { class: "awsm-job-specification-job-location" });
+  const soupElements2 = soup.findAll("div", {
+    class: "awsm-job-specification-job-location",
+  });
 
-  for (let i = 0; i < soupElements.length; i++) {
+  for (let i = 0; i < soupElements.length; i += 1) {
     soupElements[i].append(soupElements2[i]);
   }
   soupElements.forEach((el) => {
     const job_title = el.find("a").text;
     const job_link = el.find("a").attrs.href;
     const city = el.find("span").text.trim();
-    const job = generateJob(job_title, job_link,city);
+    const job = generateJob(job_title, job_link, city);
     jobs.push(job);
   });
   return jobs;
 };
 
+const getJobs = async () => {
+  const allJobs = [];
+  const firstPageUrl =
+    "https://drmaxromania.com/oportunitati-de-cariera/?paged=1";
+  const scraper = new Scraper(firstPageUrl);
+  const type = "HTML";
+  const soup = await scraper.get_soup(type);
+  const soupElements3 = soup.findAll("a", { class: "page-numbers" });
+  const pagination = [];
+  soupElements3.forEach((el) => {
+    const page = el.text;
+    pagination.push(page);
+  });
+  const totalPages = Number(pagination[2]);
+  for (let page = 1; page <= totalPages; page += 1) {
+    const pageUrl = `https://drmaxromania.com/oportunitati-de-cariera/?paged=${page}`;
+    const pageJobs = await getJobBatch(pageUrl);
+    allJobs.push(...pageJobs);
+  }
+  return allJobs;
+};
+
 const getParams = () => {
   const company = "Dr.Max";
-  const logo = "https://drmaxromania.com/wp-content/uploads/2023/03/drmaxromania-logo-768x136.png";
+  const logo =
+    "https://drmaxromania.com/wp-content/uploads/2023/03/drmaxromania-logo-768x136.png";
   const apikey = process.env.KNOX;
   const params = {
     company,
     logo,
     apikey,
   };
-  return params
+  return params;
 };
 
 const run = async () => {
-  let allJobs = [];
-  const firstPageUrl = "https://drmaxromania.com/oportunitati-de-cariera/?paged=1";
-  const scraper = new Scraper(firstPageUrl);
-  const type = "HTML";
-  const soup = await scraper.get_soup(type);
-  const soupElements3 = soup.findAll("a", { class: "page-numbers" });
-  pagination = []
-  soupElements3.forEach((el) => {
-    const page = el.text;
-    pagination.push(page); 
-  })
-  const totalPages = Number(pagination[2]);
-  for (let page = 1; page <= totalPages; page++) {
-    const pageUrl = `https://drmaxromania.com/oportunitati-de-cariera/?paged=${page}`;
-    const pageJobs = await getJobs(pageUrl);
-    allJobs.push(...pageJobs);
-  }
+  const allJobs = await getJobs();
   const params = getParams();
   await postApiPeViitor(allJobs, params);
-  
 };
 
 if (require.main === module) {
@@ -67,4 +76,3 @@ if (require.main === module) {
 }
 
 module.exports = { run, getJobs, getParams }; // this is needed for our unit test job
-
