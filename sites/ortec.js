802416a8bd08391e353ba11b59e96db54be2266e
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2023-08-28 12:19:20 +0300

    Fixed Ortec scraper

--- sites/ortec.js
+++ sites/ortec.js
@@ -1,48 +1,72 @@
-"use strict";
-const scraper = require("../peviitor_scraper.js");
-const uuid = require("uuid");
-const company = { company: "Ortec" };
-let finalJobs = [];
-const apiKey = process.env.KNOX
-const url = 'https://ortec.com/api/career'
-fetch(url, {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'
-  }
-})
-  .then(response => response.json())
-  .then(responseData => {
+const { Scraper, postApiPeViitor } = require("peviitor_jsscraper");
+const https = require("https");
 
-    const filteredArray = responseData
-      .filter(obj => obj.country === 'Romania')
-      .map(({ title, jobId }) => ({ title, jobId }));
+const urls = {
+  Australia: "https://ortec.com/api/pages/en/careers/our-locations/australia",
+  Denmark: "https://ortec.com/api/pages/en/careers/our-locations/denmark",
+  Greece: "https://ortec.com/api/pages/en/careers/our-locations/greece",
+  Romania: "https://ortec.com/api/pages/en/careers/our-locations/romania",
+  Belgium: "https://ortec.com/api/pages/en/careers/our-locations/belgium",
+  "United States":
+    "https://ortec.com/api/pages/en/careers/our-locations/united-states",
+  France: "https://ortec.com/api/pages/en/careers/our-locations/france",
+  Italy: "https://ortec.com/api/pages/en/careers/our-locations/italy",
+  Netherlands:
+    "https://ortec.com/api/pages/en/careers/our-locations/netherlands",
+  Brazil: "https://ortec.com/api/pages/en/careers/our-locations/brazil",
+  Germany: "https://ortec.com/api/pages/en/careers/our-locations/germany",
+  Poland: "https://ortec.com/api/pages/en/careers/our-locations/poland",
+  "United Kingdom":
+    "https://ortec.com/api/pages/en/careers/our-locations/united-kingdom",
+};
 
-    filteredArray.forEach(obj => {
-      const id = uuid.v4();
-      const jobTitle = obj.title;
-      const jobId = obj.jobId;
-      finalJobs.push({
-        id: id,
-        job_title: jobTitle,
-        job_link: "https://ortec.com/en/careers/find-jobs/career/jobs?id="+ jobId,
-        company: company.company,
-        city: 'Bucuresti',
-        country: "Romania",
+const generateJob = (job_title, job_link, country, city) => ({
+  job_title,
+  job_link,
+  country,
+  city,
+});
+
+const getJobs = async () => {
+  const jobs = [];
+  for (const [key, value] of Object.entries(urls)) {
+    const scraper = new Scraper(value);
+    const httpsAgent = new https.Agent({ rejectUnauthorized: false });
+    scraper.config.httpsAgent = httpsAgent;
+    const json = await scraper.get_soup("JSON");
+    const items = json.sections[4].jobs;
+
+    if (items) {
+      items.forEach((job) => {
+        const job_obj = generateJob(job.title, job.jobURL, key, job.subtitle);
+        jobs.push(job_obj);
       });
-    });
-    console.log(finalJobs.length);
-  }).then(() => {
-    console.log(JSON.stringify(finalJobs, null, 2));
+    }
+  }
+  return jobs;
+};
 
-    scraper.postApiPeViitor(finalJobs, company, apiKey);
+const getParams = () => {
+  const company = "Ortec";
+  const logo =
+    "https://media.academictransfer.com/LT8OEP2nAexUPaM9-WfgcP488FM=/fit-in/490x162/filters:upscale():fill(white)/logos/ortec-en-wide.jpg";
+  const apikey = process.env.APIKEY;
+  const params = {
+    company,
+    logo,
+    apikey,
+  };
+  return params;
+};
 
-    let logo = "https://media.academictransfer.com/LT8OEP2nAexUPaM9-WfgcP488FM=/fit-in/490x162/filters:upscale():fill(white)/logos/ortec-en-wide.jpg";
+const run = async () => {
+  const jobs = await getJobs();
+  const params = getParams();
+  postApiPeViitor(jobs, params);
+};
 
-    let postLogo = new scraper.ApiScraper(
-      "https://api.peviitor.ro/v1/logo/add/"
-    );
-    postLogo.headers.headers["Content-Type"] = "application/json";
-    postLogo.post(JSON.stringify([{ id: company.company, logo: logo }]));
-  });
+if (require.main === module) {
+  run();
+}
+
+module.exports = { run, getJobs, getParams }; // this is needed for our unit test job
