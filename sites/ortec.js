5583d138aa975e01ff5c8ba10e5e5f1c4ef33218
Codetamer333 <kattalynho_no1@yahoo.com>
2023-09-13 23:17:24 +0300

    adding changes

--- sites/ortec.js
+++ sites/ortec.js
@@ -37,9 +37,7 @@ fetch(url, {
 
     let logo = "https://media.academictransfer.com/LT8OEP2nAexUPaM9-WfgcP488FM=/fit-in/490x162/filters:upscale():fill(white)/logos/ortec-en-wide.jpg";
 
-    let postLogo = new scraper.ApiScraper(
-      "https://content.energage.com/company-images/61925/logo.png"
-    );
+    let postLogo = new scraper.ApiScraper("https://api.peviitor.ro/v1/logo/add/");
     postLogo.headers.headers["Content-Type"] = "application/json";
     postLogo.post(JSON.stringify([{ id: company.company, logo: logo }]));
   });
