49ad9b532ee8ef0e22dca2b9c3850afe5ba5dfbc
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2023-10-09 00:02:10 +0300

    Fixed Agricover

--- sites/agricover.js
+++ sites/agricover.js
@@ -1,45 +1,63 @@
-"use strict";
-const scraper = require("../peviitor_scraper.js");
-const uuid = require("uuid");
+const { Scraper, postApiPeViitor } = require("peviitor_jsscraper");
+const { getTownAndCounty } = require("../getTownAndCounty.js");
 
-const url = "https://agricover.ro/cariere";
+const generateJob = (job_title, job_link, city) => ({
+  job_title,
+  job_link,
+  country: "Romania",
+  city,
+});
 
-const company = { company: "Agricover" };
-let finalJobs = [];
+const getJobs = async () => {
+  const url = "https://agricover.ro/cariere";
+  const scraper = new Scraper(url);
 
-const s = new scraper.Scraper(url);
+  const res = await scraper.get_soup("HTML");
+  const items = res.find("div", { class: "careers-list" }).findAll("div");
 
-s.soup
-  .then((soup) => {
-    const jobs = soup.find("div", { class: "careers-list" }).findAll("div");
-
-    jobs.forEach((job) => {
-      const id = uuid.v4();
-      const job_title = job.find("h3").find("span").text.trim();
-      const job_link = "https://agricover.ro" + job.find("a").attrs.href;
-      const city = job.find("h5").find("span").text.trim();
-
-      finalJobs.push({
-        id: id,
-        job_title: job_title,
-        job_link: job_link,
-        company: company.company,
-        city: city,
-        country: "Romania",
+  const jobs = [];
+  items.forEach((item) => {
+    let city = [];
+    const job_title = item.find("h3").text.trim();
+    const job_link =  "https://agricover.ro" + item.find("a").attrs.href;
+    const citys = item.find("h5").text.split(" ")
+    citys.forEach((c) => {
+      const replacedChars = ["(", ")", ",", "."];
+      replacedChars.forEach((char) => {
+        c = c.replace(char, "");
       });
+      if (getTownAndCounty(c).foudedTown) {
+        city.push(getTownAndCounty(c).foudedTown);
+      }
     });
-  })
-  .then(() => {
-    console.log(JSON.stringify(finalJobs, null, 2));
-
-    scraper.postApiPeViitor(finalJobs, company);
-
-    let logo =
-      "https://agricover.ro/Files/Images/AgricoverCorporate/logo/svg/logo-positive.svg";
-
-    let postLogo = new scraper.ApiScraper(
-      "https://api.peviitor.ro/v1/logo/add/"
-    );
-    postLogo.headers.headers["Content-Type"] = "application/json";
-    postLogo.post(JSON.stringify([{ id: company.company, logo: logo }]));
+    const job = generateJob(job_title, job_link, city);
+    jobs.push(job);
   });
+
+  return jobs;
+};
+
+const getParams = () => {
+  const company = "Agricover";
+  const logo =
+    "https://agricover.ro/Files/Images/AgricoverCorporate/logo/svg/logo-positive.svg";
+  const apikey = process.env.APIKEY;
+  const params = {
+    company,
+    logo,
+    apikey,
+  };
+  return params;
+};
+
+const run = async () => {
+  const jobs = await getJobs();
+  const params = getParams();
+  postApiPeViitor(jobs, params);
+};
+
+if (require.main === module) {
+    run();
+}
+
+module.exports = { run, getJobs, getParams }; // this is needed for our unit test job
