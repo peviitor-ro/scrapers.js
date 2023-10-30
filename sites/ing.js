cc06643eac0ba455c7c3ebad3c16f4141a3b2bad
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2023-10-31 00:13:12 +0200

    Partial Fixed ing scraper

--- sites/ing.js
+++ sites/ing.js
@@ -1,8 +1,8 @@
 "use strict";
 const scraper = require("../peviitor_scraper.js");
-const uuid = require("uuid");
 
-const url = "https://www.ing.jobs/romania/posturi-vacante.htm?start=0";
+const url =
+  "https://www.ing.jobs/global/careers/job-opportunities.htm?location_level_1=RO&start=0";
 const company = { company: "ING" };
 
 const s = new scraper.Scraper(url);
@@ -18,23 +18,25 @@ s.soup.then((soup) => {
 
   postLogo.post(JSON.stringify([{ id: "ING", logo: logo }]));
 
-  const totalJobs =
-    parseInt(
-      soup
-        .find("div", { class: "careers-search-results" })
-        .findAll("h2")[2]
-        .findAll("strong")[1]
-        .text.trim()
-    );
+  const totalJobs = parseInt(
+    soup
+      .find("div", { class: "careers-search-results" })
+      .findAll("h2")[2]
+      .findAll("strong")[1]
+      .text.trim()
+  );
 
   const steps = scraper.range(0, totalJobs, 50);
 
   let jobs = [];
 
   let fetchData = () => {
-    return new Promise((resolve, reject) => {
-      for (let i = 0; i < steps.length; i++) {
-        let url = `https://www.ing.jobs/romania/posturi-vacante.htm?start=${steps[i]}`;
+    return new Promise((resolve) => {
+      for (let step in steps) {
+
+        let url = `https://www.ing.jobs/global/careers/job-opportunities.htm?location_level_1=RO&start=${
+          step * 50
+        }`;
         const s = new scraper.Scraper(url);
 
         s.soup.then((soup) => {
@@ -43,17 +45,16 @@ s.soup.then((soup) => {
             .findAll("div", { class: "careers-search-result" });
 
           results.forEach((job) => {
-            const id = uuid.v4();
             const job_title = job.find("h3").text.trim();
             const job_link =
               "https://www.ing.jobs" + job.find("h3").find("a").attrs.href;
 
             jobs.push({
-              id: id,
               job_title: job_title,
               job_link: job_link,
               company: company.company,
-              city: "Romania",
+              city: "Bucuresti",
+              county: "Bucuresti",
               country: "Romania",
             });
           });
