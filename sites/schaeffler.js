a9387edd2d67c3be5230528de82829996bb2a8e1
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2023-12-21 07:58:37 +0200

    Fixed Schaeffler

--- sites/schaeffler.js
+++ sites/schaeffler.js
@@ -1,6 +1,31 @@
 "use strict";
 const scraper = require("../peviitor_scraper.js");
-const uuid = require("uuid");
+const { translate_city } = require("../utils.js");
+const { getTownAndCounty } = require("../getTownAndCounty.js");
+
+async function get_aditonal_city(url) {
+  const s = new scraper.Scraper(url);
+  const soup = await s.soup;
+  let locations = soup
+    .findAll("span", { class: "jobGeoLocation" })
+    .map((span) => span.text.split(",")[0].trim());
+
+  let cities = [];
+  let counties = [];
+
+  locations.forEach((location) => {
+    const city = translate_city(location.trim());
+
+    const { foudedTown, county } = getTownAndCounty(city);
+
+    if (foudedTown && county) {
+      cities.push(foudedTown);
+      counties.push(county);
+    }
+  });
+
+  return { foundedTown: cities, county: counties };
+}
 
 const url =
   "https://jobs.schaeffler.com/search/?createNewAlert=false&q=&locationsearch=Romania&optionsFacetsDD_country=&optionsFacetsDD_customfield1=&optionsFacetsDD_shifttype=&optionsFacetsDD_lang=&optionsFacetsDD_customfield2=&optionsFacetsDD_customfield4=";
@@ -25,28 +50,35 @@ s.soup.then((soup) => {
         s.soup.then((soup) => {
           let results = soup.findAll("li", { class: "job-tile" });
 
-          results.forEach((job) => {
-            const id = uuid.v4();
+          const jobs_elements = results.map(async (job) => {
             const job_title = job.find("a").text.trim();
             const job_link =
               "https://jobs.schaeffler.com" + job.find("a").attrs.href;
-            const city = job
-              .find("div", { class: "city" })
-              .find("div")
-              .text.trim();
+            const city = translate_city(
+              job.find("div", { class: "city" }).find("div").text.trim()
+            );
+
+            let location_obj = getTownAndCounty(city);
+
+            if (!location_obj.county) {
+              location_obj = await get_aditonal_city(job_link);
+            }
 
             jobs.push({
-              id: id,
               job_title: job_title,
               job_link: job_link,
               company: company.company,
-              city: city,
               country: "Romania",
+              city: location_obj.foudedTown,
+              county: location_obj.county,
             });
           });
-          if (jobs.length === totalJobs) {
-            resolve(jobs);
-          }
+
+          Promise.all(jobs_elements).then(() => {
+            if (jobs.length === totalJobs) {
+              resolve(jobs);
+            }
+          });
         });
       }
     });
