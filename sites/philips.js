9b82cb8433b50e44947d29d6e20ba9779af90bd4
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2023-12-30 19:19:35 +0200

    Fixed Philips

--- sites/philips.js
+++ sites/philips.js
@@ -3,6 +3,39 @@ const scraper = require("../peviitor_scraper.js");
 const { getTownAndCounty } = require("../getTownAndCounty.js");
 const { translate_city } = require("../utils.js");
 
+async function get_aditonal_city(url) {
+  const s = new scraper.ApiScraper(url);
+  s.headers.headers["Content-Type"] = "application/json";
+  s.headers.headers["Accept"] = "application/json";
+  const soup = await s.get();
+  let locations = [];
+
+  try {
+    locations = [
+      ...soup.jobPostingInfo.additionalLocations,
+      soup.jobPostingInfo.location,
+    ];
+  } catch (error) {
+    locations = [soup.jobPostingInfo.location];
+  }
+
+  let location_elements = false;
+
+  locations.map((location) => {
+    const { foudedTown, county } = getTownAndCounty(
+      translate_city(location.toLowerCase().trim())
+    );
+    if (foudedTown && county) {
+      location_elements = {
+        foudedTown: foudedTown,
+        county: county,
+      };
+    }
+  });
+
+  return location_elements;
+}
+
 const url =
   "https://philips.wd3.myworkdayjobs.com/wday/cxs/philips/jobs-and-careers/jobs";
 
@@ -46,37 +79,46 @@ s.post(data).then((response) => {
   let jobs = [];
 
   fetchData()
-    .then((finalJobs) => {
-      finalJobs.forEach((job) => {
-        const job_title = job.title;
-        const job_link =
-          "https://philips.wd3.myworkdayjobs.com/en-US/jobs-and-careers" +
-          job.externalPath;
-        const city = job.locationsText.split(",")[0].trim();
+    .then(async (finalJobs) => {
+      await Promise.all(
+        finalJobs.map(async (job) => {
+          const job_title = job.title;
+          const job_link =
+            "https://philips.wd3.myworkdayjobs.com/en-US/jobs-and-careers" +
+            job.externalPath;
+          const city = job.locationsText.split(",")[0].trim();
 
-        const { foudedTown, county } = getTownAndCounty(
-          translate_city(city.toLowerCase())
-        );
+          const { foudedTown, county } = getTownAndCounty(
+            translate_city(city.toLowerCase())
+          );
 
-        if (foudedTown && county) {
-          jobs.push({
+          const job_element = {
             job_title: job_title,
             job_link: job_link,
             company: company.company,
             country: "Romania",
-            city: foudedTown,
-            county: county,
-          });
-        } else {
-          jobs.push({
-            job_title: job_title,
-            job_link: job_link,
-            company: company.company,
-            country: "Romania",
-            remote: ["Remote"],
-          });
-        }
-      });
+          };
+
+          if (foudedTown && county) {
+            job_element.city = foudedTown;
+            job_element.county = county;
+          } else {
+            const location = await get_aditonal_city(
+              "https://philips.wd3.myworkdayjobs.com/wday/cxs/philips/jobs-and-careers" +
+                job.externalPath
+            );
+
+            if (location) {
+              job_element.city = location.foudedTown;
+              job_element.county = location.county;
+            }
+
+            job_element.remote = ["Remote"];
+          }
+
+          jobs.push(job_element);
+        })
+      );
     })
     .then(() => {
       console.log(JSON.stringify(jobs, null, 2));
