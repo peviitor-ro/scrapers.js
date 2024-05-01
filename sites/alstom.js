703d3b9672091977388d19791da260cc3d71032e
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2024-05-02 00:05:26 +0300

    Refactor utils.js and add Aera , Abbvie, Airbus, Alstom and Alten scrapers

---
+++ sites/alstom.js
@@ -0,0 +1,79 @@
+const { translate_city } = require("../utils.js");
+const {
+  Scraper,
+  postApiPeViitor,
+  generateJob,
+  getParams,
+  range,
+} = require("peviitor_jsscraper");
+const { Counties } = require("../getTownAndCounty.js");
+
+const _counties = new Counties();
+
+const getJobs = async () => {
+  const url = "https://jobsearch.alstom.com/search/?q=&locationsearch=Romania";
+  const scraper = new Scraper(url);
+  const res = await scraper.get_soup("HTML");
+
+  const totalJobs = parseInt(
+    res.find("span", { class: "paginationLabel" }).findAll("b")[1].text
+  );
+  const step = 25;
+
+  const pages = range(0, totalJobs, step);
+
+  const items = [];
+
+  if (totalJobs > step) {
+    await Promise.all(
+      pages.map(async (page) => {
+        const url = `https://jobsearch.alstom.com/search/?q=&locationsearch=Romania&startrow=${page}`;
+        const scraper = new Scraper(url);
+        const res = await scraper.get_soup("HTML");
+        items.push(...res.find("tbody").findAll("tr"));
+      })
+    );
+  } else {
+    items.push(...res.find("tbody").findAll("tr"));
+  }
+
+  const jobs = [];
+
+  await Promise.all(
+    items.map(async (item) => {
+      let cities = [];
+      let counties = [];
+      const job_title = item.find("a").text.trim();
+      const job_link = "https://jobsearch.alstom.com" + item.find("a").attrs.href;
+      const country = "Romania";
+      const city = translate_city(
+        item.find("span", { class: "jobLocation" }).text.split(",")[0].trim()
+      );
+
+      const { city: c, county: co } = await _counties.getCounties(city);
+      if (c) {
+        cities.push(c);
+        counties = [...new Set([...counties, ...co])];
+      }
+
+      const job = generateJob(job_title, job_link, country, cities, counties);
+      jobs.push(job);
+    })
+  );
+  return jobs;
+};
+
+const run = async () => {
+  const company = "Alstom";
+  const logo =
+    "https://rmkcdn.successfactors.com/44ea18da/ff6f3396-32e1-421d-915a-5.jpg";
+  const jobs = await getJobs();
+  const params = getParams(company, logo);
+  postApiPeViitor(jobs, params);
+}
+
+if (require.main === module) {
+  run();
+}
+
+module.exports = { run, getJobs, getParams };
---
+++ sites/alten.js
@@ -0,0 +1,142 @@
+const { translate_city } = require("../utils.js");
+const {
+  Scraper,
+  postApiPeViitor,
+  generateJob,
+  getParams,
+  range,
+} = require("peviitor_jsscraper");
+const { Counties } = require("../getTownAndCounty.js");
+
+const _counties = new Counties();
+
+const getJobs = async () => {
+  let url = "https://careers.altenromania.ro/jds/1";
+  const scraper = new Scraper(url);
+  let res = await scraper.get_soup("JSON");
+
+  const pages = JSON.parse(res.success.message).pager.pageCount;
+  const items = [];
+  const jobs = [];
+
+  await Promise.all(
+    range(1, pages, 1).map(async (i) => {
+      url = "https://careers.altenromania.ro/jds/" + i;
+      scraper.url = url;
+      const res = await scraper.get_soup("JSON");
+      items.push(...JSON.parse(res.success.message).recordList);
+    })
+  );
+
+  await Promise.all(
+    items.map(async (job) => {
+      const job_title = job.titlu;
+      const job_link = "https://careers.altenromania.ro/job/" + job.id;
+      const city = job.locatie;
+      const country = "Romania";
+      const remote = [];
+
+      const job_obj = generateJob(job_title, job_link, country);
+
+      if (city.includes("Remote")) {
+        remote.push("Remote");
+        job_obj.remote = remote;
+      } else {
+        const { city: c, county: co } = await _counties.getCounties(city);
+        if (c) {
+          job_obj.city = c;
+          job_obj.county = co;
+        }
+      }
+
+      jobs.push(job_obj);
+    })
+  );
+
+  return jobs;
+};
+
+const run = async () => {
+  const company = "Alten";
+  const logo = "https://careers.altenromania.ro/assets/img/svgs/logo.svg";
+  const jobs = await getJobs();
+  const params = getParams(company, logo);
+  postApiPeViitor(jobs, params);
+};
+
+if (require.main === module) {
+  run();
+}
+
+module.exports = { run, getJobs, getParams };
+// const { Scraper, postApiPeViitor } = require("peviitor_jsscraper");
+// const { getTownAndCounty } = require("../getTownAndCounty.js");
+// const { translate_city } = require("../utils.js");
+
+// const generateJob = (job_title, job_link, remote, city, county) => ({
+//   job_title,
+//   job_link,
+//   country: "Romania",
+//   city,
+//   county,
+//   remote,
+// });
+
+// const getJobs = async () => {
+//   let url = "https://careers.altenromania.ro/jds/1";
+//   const jobs = [];
+
+//   const scraper = new Scraper(url);
+//   let res = await scraper.get_soup("JSON");
+
+//   const pages = JSON.parse(res.success.message).pager.pageCount;
+
+//   for (let i = 1; i <= pages; i++) {
+//     const jobs_elements = JSON.parse(res.success.message).recordList;
+//     jobs_elements.forEach((job) => {
+//       const job_title = job.titlu;
+//       const job_link = "https://careers.altenromania.ro/job/" + job.id;
+//       const city = job.locatie;
+//       const remote = [];
+
+//       if (city.includes("Remote")) {
+//         remote.push("Remote");
+//         jobs.push(generateJob(job_title, job_link, remote));
+//       } else {
+//         const { foudedTown, county } = getTownAndCounty(
+//           translate_city(city.toLowerCase())
+//         );
+//         jobs.push(generateJob(job_title, job_link, remote, foudedTown, county));
+//       }
+//     });
+//     url = "https://careers.altenromania.ro/jds/" + (i + 1);
+//     scraper.url = url;
+//     res = await scraper.get_soup("JSON");
+//   }
+
+//   return jobs;
+// };
+
+// const getParams = () => {
+//   const company = "Alten";
+//   const logo = "https://careers.altenromania.ro/assets/img/svgs/logo.svg";
+//   const apikey = process.env.APIKEY;
+//   const params = {
+//     company,
+//     logo,
+//     apikey,
+//   };
+//   return params;
+// };
+
+// const run = async () => {
+//   const jobs = await getJobs();
+//   const params = getParams();
+//   postApiPeViitor(jobs, params);
+// };
+
+// if (require.main === module) {
+//   run();
+// }
+
+// module.exports = { run, getJobs, getParams }; // this is needed for our unit test job
