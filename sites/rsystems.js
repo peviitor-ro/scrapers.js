cc8ddb4a644003338ef1edf2612e8646ba31016d
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2025-07-07 19:17:05 +0300

    Update RSystems job scraper to improve data extraction and pagination handling

--- sites/rsystems.js
+++ sites/rsystems.js
@@ -1,26 +1,45 @@
 const {
-  Scraper,
   postApiPeViitor,
   generateJob,
   getParams,
 } = require("peviitor_jsscraper");
 
+const Jssoup = require("jssoup").default;
+
 const getJobs = async () => {
-  let url = "https://eu.rsystems.com/category/careers/romania-bucharest/";
+  let page = 1;
   const jobs = [];
-  const scraper = new Scraper(url);
 
-  let res = await scraper.get_soup("HTML");
-  let items = res.findAll("div", { class: "uael-post__inner-wrap" });
-
-  for (const item of items) {
-    const job_title = item.find("h3").text.trim();
-    const job_link = item.find("a").attrs.href;
-    const city = "Bucuresti";
-    const county = "Bucuresti";
-
-    jobs.push(generateJob(job_title, job_link, city, county));
+  const requestOptions = {
+    method: "GET",
+    redirect: "follow",
   };
+
+  let data = await fetch(
+    "https://eu.rsystems.com/category/careers/romania-bucharest/",
+    requestOptions
+  ).then((response) => response.text());
+
+  let res = new Jssoup(data);
+  let items = res.findAll("div", { class: "uael-post-wrapper" });
+
+  while (items.length > 0) {
+    for (const item of items) {
+      const job_title = item.find("h3").text.trim();
+      const job_link = item.find("a").attrs.href;
+      const city = "Bucuresti";
+      const county = "Bucuresti";
+
+      jobs.push(generateJob(job_title, job_link, "Romania", city, county));
+    }
+    page += 1;
+    let data = await fetch(
+      `https://eu.rsystems.com/category/careers/romania-bucharest/page/${page}/`,
+      requestOptions
+    ).then((response) => response.text());
+    res = new Jssoup(data);
+    items = res.findAll("div", { class: "uael-post-wrapper" });
+  }
   return jobs;
 };
 
