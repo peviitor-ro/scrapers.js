fa58880d33b9b448ed9e220d7bac2515de02e385
lalalaurentiu <67306273+lalalaurentiu@users.noreply.github.com>
2026-08-21 21:57:43 +0000

    chore: update scraper outputs

--- sites/poppasociatii.js
+++ sites/poppasociatii.js
@@ -1,21 +1,131 @@
 const {
-  Scraper,
   postApiPeViitor,
   generateJob,
   getParams,
 } = require("peviitor_jsscraper");
+const Jssoup = require("jssoup").default;
+const axios = require("axios");
+const vm = require("vm");
 const { Counties } = require("../getTownAndCounty.js");
 
 const _counties = new Counties();
 
+const headers = {
+  "User-Agent":
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36",
+  Accept:
+    "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+};
+
+const collectCookies = (response, jar) => {
+  const cookies = (response.headers["set-cookie"] || []).map(
+    (cookie) => cookie.split(";")[0],
+  );
+  return [...jar, ...cookies];
+};
+
+const solveChallenge = (html) => {
+  const scripts = [...html.matchAll(/<script>([\s\S]*?)<\/script>/g)].map(
+    (match) => match[1],
+  );
+  const script = scripts.find((code) => code.includes("submit"));
+  if (!script) {
+    return null;
+  }
+
+  let captured = null;
+  const makeElement = () => {
+    const element = { children: [], style: "", type: "" };
+    element.appendChild = (child) => element.children.push(child);
+    Object.defineProperty(element, "submit", {
+      value() {
+        captured = {
+          action: element.action,
+          fields: element.children.map((child) => ({
+            name: child.name,
+            value: String(child.value),
+          })),
+        };
+      },
+    });
+    return element;
+  };
+
+  const container = makeElement();
+  const document = {
+    getElementById: () => container,
+    createElement: makeElement,
+    attachEvent: undefined,
+    addEventListener: (_event, callback) => callback(),
+  };
+  const userAgent =
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36";
+  const windowObject = {
+    navigator: {
+      userAgent,
+      appVersion: userAgent.replace("Mozilla/", "Mozilla/"),
+      language: "en-US",
+      languages: ["en-US", "en"],
+    },
+    outerHeight: 1080,
+    outerWidth: 1920,
+    addEventListener: (_event, callback) => callback(),
+    document,
+  };
+  const sandbox = {
+    window: windowObject,
+    document,
+    setTimeout: (callback) => callback(),
+    console,
+    XMLHttpRequest: class {
+      open() {}
+      send() {}
+    },
+    URLSearchParams,
+  };
+  vm.createContext(sandbox);
+  vm.runInContext(script, sandbox);
+  return captured;
+};
+
+const fetchPage = async (url) => {
+  const firstResponse = await axios.get(url, { headers });
+  let cookies = collectCookies(firstResponse, []);
+  let { data: html } = firstResponse;
+
+  if (html.includes("wsidchk")) {
+    const form = solveChallenge(html);
+    if (form) {
+      const origin = new URL(url).origin;
+      const query = new URLSearchParams();
+      form.fields.forEach((field) => query.append(field.name, field.value));
+
+      const verifyResponse = await axios.get(
+        `${origin}${form.action}?${query.toString()}`,
+        {
+          headers: { ...headers, Cookie: cookies.join("; "), Referer: url },
+          maxRedirects: 0,
+          validateStatus: () => true,
+        },
+      );
+      cookies = collectCookies(verifyResponse, cookies);
+
+      const finalResponse = await axios.get(url, {
+        headers: { ...headers, Cookie: cookies.join("; ") },
+      });
+      html = finalResponse.data;
+    }
+  }
+  return html;
+};
+
 const getJobs = async () => {
   const url = "https://www.p-a.ro/cariere/";
 
-  const scraper = new Scraper(url);
-
   const jobs = [];
 
-  const soup = await scraper.get_soup("HTML");
+  const html = await fetchPage(url);
+  const soup = new Jssoup(html);
 
   const items = soup.findAll("div", { class: "post-wrap" });
 
